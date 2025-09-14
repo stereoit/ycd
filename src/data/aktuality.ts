d3f63cf72dbd529301708bcd0d02a4c70fa71e68
Robert Smol <robert.smol@stereoit.com>
2025-09-14 22:40:41 +0200

    feat: Create home page sections

---
+++ src/data/aktuality.ts
@@ -0,0 +1,7 @@
+export const aktuality = [
+  {
+    date: "25.4.2025",
+    title: "členská bridágda 2025",
+    body: "Srdečně zveme všechny členy našeho jachtařského klubu na tradiční jarní brigádu, která se uskuteční [datum konání brigády] od [čas začátku] v areálu našeho klubu na [místo konání]. Během této společné akce se budeme věnovat údržbě a přípravě našeho zázemí a lodí na nadcházející sezónu. Vaše pomoc a nadšení jsou pro nás neocenitelné, a proto věříme, že se v hojném počtu sejdeme, abychom společně přispěli k ještě lepšímu fungování našeho klubu a užili si při tom i trochu té jarní pohody. Občerstvení a dobrá nálada budou zajištěny! Těšíme se na vaši účast.",
+  },
+];
